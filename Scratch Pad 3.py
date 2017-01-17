72618adcc4564fd70bfa1e7ae1c276a142018bb5
Grant Draper <grant_draper@hotmail.co.uk>
2017-01-17 13:37:37 +0000

    looking at psutil module

---
+++ Scratch Pad 3.py
@@ -0,0 +1,6 @@
+#testing psutil library
+
+import psutil
+
+
+
--- Scratch pad 2.py
+++ Scratch pad 2.py
@@ -1,24 +1,24 @@
 
 """Complete working formatted operation with comments for creating filepath,
     directory, file and usedspace values - yet to be defined as a function"""
-# import os
-# from os.path import join, getsize
-#
-# #uses os.walk to scan the selected dir, returns 3 argumennts.
-# for filepath, directorys, files in os.walk("C:\\Users\Grant\Downloads"):
-#
-#     #prints the filepath argument
-#     print(filepath,)
-#
-#     """uses os.path.join to append all files in filepath to a single argument called name,
-#         os.path.getsize queries using os.stat to return the bytesize value for each segment
-#          of the name argument. sum adds the values, which is then assigned the identifier
-#          "filesize" """
-#     filesize = sum([getsize(join(filepath, name)) for name in files])
-#     print("consumes", filesize, "bytes", )
-#
-#     #the len function then counts all values in the list "files", then prints with byt
-#     print("in", (len(files)), "files", "\n ")
+import os
+from os.path import join, getsize
+
+#uses os.walk to scan the selected dir, returns 3 argumennts.
+for filepath, directorys, files in os.walk("C:\\Users\Grant\Downloads"):
+
+    #prints the filepath argument
+    print(filepath,)
+
+    """uses os.path.join to append all files in filepath to a single argument called name,
+        os.path.getsize queries using os.stat to return the bytesize value for each segment
+         of the name argument. sum adds the values, which is then assigned the identifier
+         "filesize" """
+    filesize = sum([getsize(join(filepath, name)) for name in files])
+    print("consumes", filesize, "bytes", )
+
+    #the len function then counts all values in the list "files", then prints with byt
+    print("in", (len(files)), "files", "\n ")
 
 
 
@@ -110,7 +110,8 @@ def root_info():
         #pass
 
 root_info()
-print (root_info(byte_size))
+## print (root_info(byte_size))
+print (byte_size)
 #fs = root_info("byte_size")
 #print(fs)
 #print (root_info(byte_size))
