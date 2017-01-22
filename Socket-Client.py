d8c7b19cbb9cb7aa7ebdc6a4eda7a005f835876e
Grant Draper <grant_draper@hotmail.co.uk>
2017-01-22 17:05:29 +0000

    Working on refining the os.walk function to reduce the ammount of data displayed and time taken to scan dir. added function with exception handling to client, to get the mounted drives of the whole system, and scan for the space information. works with external media also.
    Still working on the above, also tidying code and adding comments for each section.
    
    same as previous, added working code from mike for TLS handshake, created cert and added also.

--- Socket-Client.py
+++ Socket-Client.py
@@ -1,5 +1,40 @@
+#working code from mike, client side, cert created by me, see CertCreation.txt
 
-# #client side, with exception handling
+
+#!/usr/bin/env python3.5
+import socket, ssl
+
+if __name__ == "__main__":
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  ssl_sock = ssl.wrap_socket(s, ca_certs="server.crt", cert_reqs=ssl.CERT_REQUIRED)
+  ssl_sock.connect(("localhost", 7075))
+  ssl_sock.write("fuck".encode("UTF-8"))
+  stuff = ssl_sock.read()
+  print(stuff.decode("UTF-8"))
+  ssl_sock.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#client side, with exception handling
 #
 # import socket
 # import sys
@@ -29,7 +64,7 @@
 #     counter = 1
 #     if counter < (counter + 1) :
 #        s.sendall(bytes(counter, "utf8"))
-#
+
 #
 # time.sleep(1)
 # s.sendall(bytes("Grant says hi", "utf8"))
@@ -44,30 +79,30 @@
 
 
 #Connection 2
-
-import socket, sys
-
-#these are the creds of the target machine and port
-HOST = "127.0.0.1"
-PORT = 30001
-
-#this creates variable called s
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    s.connect((HOST, PORT))
-except:
-    print("Fail")
-    sys.exit()
-
-
-s.sendall(bytes("Dave says hi", "utf8"))
-
-data = s.recv(1024)
-print ('Received', repr(data))
-
-s.shutdown(socket.SHUT_RDWR)
-s.close()
+#
+# import socket, sys
+#
+# #these are the creds of the target machine and port
+# HOST = "127.0.0.1"
+# PORT = 30001
+#
+# #this creates variable called s
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# try:
+#     s.connect((HOST, PORT))
+# except:
+#     print("Fail")
+#     sys.exit()
+#
+#
+# s.sendall(bytes("Dave says hi", "utf8"))
+#
+# data = s.recv(1024)
+# print ('Received', repr(data))
+#
+# s.shutdown(socket.SHUT_RDWR)
+# s.close()
 
 
 
--- Socket-Server.py
+++ Socket-Server.py
@@ -1,47 +1,72 @@
+#working code from mike, server side, cert created by me, see CertCreation.txt
+
+
+#!/usr/bin/env python3.5
+import socket, ssl
+
+def start_listening():
+  bindsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  bindsocket.bind(('', 7075))
+  bindsocket.listen(1)
+  return bindsocket
+
+if __name__ == "__main__":
+  s = start_listening()
+  newsock, newaddr = s.accept()
+  ssl_socket = ssl.wrap_socket(newsock, server_side=True, certfile="server.crt", keyfile="server.key")
+  data = ssl_socket.read()
+  print(data.decode("UTF-8"))
+  response_data = data.upper()
+  ssl_socket.write(response_data)
+  ssl_socket.close()
+
+
+
+
 
 #server side, multiple connections
 
-import socket
-import select
-
-HOST = "127.0.0.1"
-PORT = 30001
-
-master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-master_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-master_socket.setblocking(0)
-master_socket.bind((HOST, PORT))
-master_socket.listen(1)
-
-#creates a storage area for our connections
-sockets = []
-sockets.append(master_socket)
-print("about to loop")
-while True:
-    (readable, writable, exceptional) = select.select(sockets, [], sockets)
-    #print(readable)
-    for s in readable:
-        if s is master_socket:
-            (client, _) = master_socket.accept()
-            client.setblocking(0)
-            sockets.append(client)
-        else:
-            data = s.recv(1024)
-
-            if not data:
-                s.shutdown(socket.SHUT_RDWR)
-                s.close()
-                sockets.remove(s)
-            else:
-                s.sendall(data)
-            #print ("Recieved", repr(data))
-
-
-
-
+# import socket
+# import select
+#
+# HOST = "127.0.0.1"
+# PORT = 30001
+#
+# master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# master_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# master_socket.setblocking(0)
+# master_socket.bind((HOST, PORT))
+# master_socket.listen(1)
+#
+# #creates a storage area for our connections
+# sockets = []
+# sockets.append(master_socket)
+# print("about to loop")
+# while True:
+#     (readable, writable, exceptional) = select.select(sockets, [], sockets)
+#     #print(readable)
+#     for s in readable:
+#         if s is master_socket:
+#             (client, _) = master_socket.accept()
+#             client.setblocking(0)
+#             sockets.append(client)
+#         else:
+#             data = s.recv(1024)
+#
+#             if not data:
+#                 s.shutdown(socket.SHUT_RDWR)
+#                 s.close()
+#                 sockets.remove(s)
+#             else:
+#                 s.sendall(data)
+#             #print ("Recieved", repr(data))
+#
+#
 
 
 
+#
+#
 # #Connection 2
 #
 # import socket
